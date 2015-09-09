3c0232bc9641283e689876ce4189b566dfc3603c
chenjianyi <chen@chentekiiMac.local>
2015-09-09 11:38:13 +0800

    code optimization, style add

--- page_console_log.js
+++ page_console_log.js
@@ -6,75 +6,68 @@
 ;
 (function () {
 
-  
-        var iDiv = document.createElement('div');
-        iDiv.id = 'page-console-log';
-        iDiv.style.border = '3px solid #aaa';
-        iDiv.style.padding = '10px';
-        iDiv.style.margin = '5px';
-        iDiv.style.height = '100px';
-        //iDiv.style.width = '98%';
-        iDiv.style.overflowY = 'scroll';
-        iDiv.style.background = '#fff';
-        var html = document.getElementsByTagName('html')[0];
-        html.insertBefore(iDiv, html.childNodes[0]);
-        
-        
-        var oldLog = console.log;
-        var oldDebug = console.debug;
-        var oldInfo = console.info;
-        var oldWarn = console.warn;
-        var oldError = console.error;
-        
-        console.log =   function(){
-        	var combinedArray = [oldLog,'log'].concat(Array.prototype.slice.call(arguments));
-        	hijack.apply(this,combinedArray);
+
+    var iDiv = document.createElement('div');
+    iDiv.id = 'page-console-log';
+    //iDiv.style.border = '3px solid #aaa';
+    //iDiv.style.padding = '10px';
+    //iDiv.style.margin = '5px';
+    iDiv.style.height = '300px';
+    //iDiv.style.width = '100%';
+    iDiv.style.overflowY = 'scroll';
+    iDiv.style.background = '#000';
+    iDiv.style.color = '#fff';
+    var html = document.getElementsByTagName('html')[0];
+    html.insertBefore(iDiv, html.childNodes[0]);
+
+    console.log = combineArguments(console.log, 'log');
+    console.debug = combineArguments(console.debug, 'debug');
+    console.info = combineArguments(console.info, 'info');
+    console.warn = combineArguments(console.warn, 'warn');
+    console.error = combineArguments(console.error, 'error');
+
+
+    function combineArguments(old, type) {
+        return function () {
+            var combinedArray = [old, type].concat(Array.prototype.slice.call(arguments));
+            hijackConsole.apply(this, combinedArray);
         };
-  
-        console.debug =  function(){
-        	var combinedArray = [oldDebug,'debug'].concat(Array.prototype.slice.call(arguments));
-        	hijack.apply(this,combinedArray);
-        };
-      
-        console.info =  function(){
-        	var combinedArray = [oldInfo,'info'].concat(Array.prototype.slice.call(arguments));
-        	hijack.apply(this,combinedArray);
-        };
-       
-        console.warn =  function(){
-        	var combinedArray = [oldWarn,'warn'].concat(Array.prototype.slice.call(arguments));
-        	hijack.apply(this,combinedArray);
-        };
-        
-        console.error =  function(){
-        	var combinedArray = [oldError,'error'].concat(Array.prototype.slice.call(arguments));
-        	hijack.apply(this,combinedArray);
-        };
-     
-       /* 
-      function combineArray(old,type){
-      		var combinedArray = [old,type].concat(Array.prototype.slice.call(arguments));
-        	hijack.apply(this,combinedArray);
-      }*/
-    
-    function hijack(){
-    	 var args = '';
-            for (var i = 2; i < arguments.length; i++) {
-            	var r='';
-            	try {
-            		r=JSON.stringify(arguments[i]);
-            	}catch(ex){
-            		r=ex;
-            	}
-                args += '<div style="padding-left:10px;margin:5px;">'+r+'</div>' ;
-            }
-           //location.pathname.substring(1)＋ 
-            var p = '<div style="color:red;">console.'+arguments[1]+':</div>' ;
-            iDiv.innerHTML ='<div>' + p + args + '' + '</div>' + iDiv.innerHTML;
-           // console.log(arguments[0]);
-    	 arguments[0].apply(console, arguments);
-    	 //iDiv.innerHTML =arguments[1];
     }
-    
-    
+
+    function hijackConsole() {
+        var oldMethod = arguments[0];
+        var consoleType = arguments[1];
+        var color = '#f0f0f0';
+        if (consoleType == 'debug') {
+            color = '#ADD8E6';
+        } else if (consoleType == 'info') {
+            color = 'green';
+        } else if (consoleType == 'warn') {
+            color = 'yellow';
+        } else if (consoleType == 'error') {
+            color = 'red';
+        }
+        var args = '';
+        for (var i = 2; i < arguments.length; i++) {
+            var r = '';
+            try {
+                r = JSON.stringify(arguments[i]);
+            } catch (ex) {
+                r = ex;
+            }
+            args += '<div style="padding-left:10px;margin:5px;">' + r + (i != arguments.length - 1 ? "," : "") + '</div>';
+        }
+
+
+        var p = '<div style="color:' + color + ';">console.' + consoleType + ':</div>';
+        iDiv.innerHTML = '<div style="color:' + color + ';border-bottom:1px solid #aaa;padding:8px;">' + p + args + '' + '</div>' + iDiv.innerHTML;
+
+        var args = Array.prototype.slice.call(arguments);
+        args.shift();
+        args.shift();
+
+        oldMethod.apply(console, args);
+    }
+
+
 })();
